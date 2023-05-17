0dc06db58e1c2e7014a86ec284fe05431a2c6aca
ambiti0n <fabulousfresh010@gmail.com>
2023-05-17 06:00:37 +0100

    fix availability

--- src/components/modal/Setup.tsx
+++ src/components/modal/Setup.tsx
@@ -16,7 +16,6 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import Availability from "./component/Availability";
 import Learn from "./component/Learn";
 import Speak from "./component/Speak";
 import Complete from "./component/Complete";
@@ -29,21 +28,32 @@ interface Props {
 const Setup: React.FC<Props> = ({ isOpen, onClose }) => {
   const [activeComponent, setActiveComponent] = useState<string>("learn");
 
-    return (
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-                <ModalCloseButton _hover={{ bg: 'transparent', color: '#FDD835', border: '1px solid #FDD835'}} />
-                <ModalHeader>
-                    {/* <Image src='/icons/LOGO.svg' alt='logo' /> */}
-                </ModalHeader>
-                {activeComponent === 'learn' && <Learn setActiveComponent={setActiveComponent} />}
-                {activeComponent === 'speak' && <Speak setActiveComponent={setActiveComponent} />}
-                {activeComponent === 'available' && <Availability setActiveComponent={setActiveComponent} />}
-                {activeComponent === 'complete' && <Complete setActiveComponent={setActiveComponent} onClose={onClose} />}
-            </ModalContent>
-        </Modal>
-    )
-}
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton
+          _hover={{
+            bg: "transparent",
+            color: "#FDD835",
+            border: "1px solid #FDD835",
+          }}
+        />
+        <ModalHeader>
+          {/* <Image src='/icons/LOGO.svg' alt='logo' /> */}
+        </ModalHeader>
+        {activeComponent === "learn" && (
+          <Learn setActiveComponent={setActiveComponent} />
+        )}
+        {activeComponent === "speak" && (
+          <Speak setActiveComponent={setActiveComponent} />
+        )}
+        {activeComponent === "complete" && (
+          <Complete setActiveComponent={setActiveComponent} onClose={onClose} />
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default Setup;
